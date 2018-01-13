8abc712f7e05d3618c57fda109269288c3384be2
Candice Mayes <ca.mayes239@gmail.com>
2018-01-12 21:45:03 -0500

    added business component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Business from './components/Business';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Business />, document.getElementById('root'));
 registerServiceWorker();
