d4709e128ebb084b8cbc0aa3bed9c2d994f296b3
Candice Mayes <ca.mayes239@gmail.com>
2018-01-14 12:53:31 -0500

    passed props to business component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Business from './components/Business';
 
-ReactDOM.render(<Business />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
